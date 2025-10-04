9c80c8b49ac0ad1028b397d81f479bb001366e3f
dhananjay-jadhav <jadhav9260@gmail.com>
2025-10-04 18:26:05 +0530

    Fix test cases

--- libs/users-api/src/lib/users.resolver.spec.ts
+++ libs/users-api/src/lib/users.resolver.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { UserService } from '@jobber/users';
+import { AuthDbService } from '@jobber/auth-db';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      providers: [UsersResolver, UserService, AuthDbService],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
