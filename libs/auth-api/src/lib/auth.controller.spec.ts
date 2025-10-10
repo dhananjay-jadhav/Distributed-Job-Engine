3cf7a33c1fb1aea5b6f80c706651036af1f93687
dhananjay-jadhav <jadhav9260@gmail.com>
2025-10-10 15:39:18 +0530

    Fix unit test cases

--- libs/auth-api/src/lib/auth.controller.spec.ts
+++ libs/auth-api/src/lib/auth.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '@jobber/users';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
       controllers: [AuthController],
     }).compile();
 
--- libs/jobs-api/src/lib/jobs.resolver.spec.ts
+++ libs/jobs-api/src/lib/jobs.resolver.spec.ts
@@ -1,13 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobsResolver } from './jobs.resolver';
 import { JobsServiceModule } from '@jobber/jobs-service';
+import { AUTH_PACKAGE_NAME } from '@jobber/proto';
+import { ClientsModule } from '@nestjs/microservices';
+import { LoggerModule } from 'nestjs-pino';
 
 describe('JobsResolver', () => {
   let resolver: JobsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [JobsServiceModule],
+      imports: [
+        LoggerModule.forRoot(),
+        JobsServiceModule,
+        ClientsModule.register([
+          {
+            name: AUTH_PACKAGE_NAME,
+          },
+        ]),
+      ],
       providers: [JobsResolver],
     }).compile();
 
